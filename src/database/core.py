56bbe9d54720421329b4ee7c5a3f1583f465328b
rayaneatd <179324378+rayaneatd@users.noreply.github.com>
2026-08-27 16:50:56 +0100

    feat (execute_sql functions): optimized execute_sql_from_string / execute_sql_from_file for multistatement queries to avoid future problems

--- src/database/core.py
+++ src/database/core.py
@@ -1,3 +1,5 @@
+from src.utils.alerting import AlertLevel
+from src.utils.alerting import log_to_discord
 import os
 import polars as pl
 from psycopg_pool import ConnectionPool
@@ -40,7 +42,8 @@ def _execute(
 def execute_sql_from_string(
     pool: ConnectionPool, 
     query: str | sql.SQL, 
-    params: Sequence[Any] | Mapping[str, Any] | None = None
+    params: Sequence[Any] | Mapping[str, Any] | None = None,
+    is_multistatement: bool = False
 ) -> None:
     """
     Executes a raw SQL string within a transaction.
@@ -49,8 +52,30 @@ def execute_sql_from_string(
         pool: The database connection pool.
         query: The SQL query to execute.
         params: The parameters to use for the query.
+        is_multistatement: are there multiple queries ? 
+    NOTE: 
+        - IF is_multistatement is True, ANY PARAMETER WILL BE IGNORED !!!!
+        - IF is_multistatement is True, Query must be a string
     """
-    _execute(pool, query, params)
+
+    if is_multistatement:
+
+        if not isinstance(query, str):
+            log_to_discord('Query must be a string in multi-statement run', AlertLevel.ERROR)
+
+            raise TypeError(f"is_multistatement=True attend un str, pas {type(query)} -> c'est ton DDL")
+        else:
+            statements = [s.strip() for s in query.split(";\n") if s.strip()]
+
+            if params is not None:
+                log_to_discord("PARAMETERS ARE IGNORED !", AlertLevel.WARNING)
+
+            for statement in statements:
+                if not statement.endswith(';'):
+                    statement += ';'
+                _execute(pool, statement)
+    else:
+        _execute(pool, query, params)
 
 
 # functions to execute sql queries from a file
@@ -68,7 +93,10 @@ def execute_sql_from_file(pool: ConnectionPool, path: str) -> None:
     with open(path, "r", encoding="utf-8") as f:
         query_str = f.read()
     
-    _execute(pool, query_str)
+    statements = [s.strip() for s in query_str.split(";\n") if s.strip()]
+
+    for statement in statements:
+        _execute(pool, statement)
 
 
 # function to read from the database
