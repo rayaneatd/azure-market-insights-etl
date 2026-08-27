ce12e4faf94da862cf6118c85aa2d8dcdd7db206
rayaneatd <179324378+rayaneatd@users.noreply.github.com>
2026-08-27 16:57:40 +0100

    feat(analytics): ensure_schema is now defined and will use the "build_pg_query" function to auto-generate every schema in the analytics layer on every run. - historisation with scd2 is also handled very well - we also initialized the "run_dq_checks" func for future data quality checks

--- src/database/analytics.py
+++ src/database/analytics.py
@@ -1,9 +1,32 @@
+# 1. third party
+import polars as pl
 from azure.storage.blob import BlobServiceClient
 from azure.storage.filedatalake import DataLakeServiceClient
 from psycopg_pool import ConnectionPool
 
+# 2. local
+from src.igdb.models import * #! this is important
+from src.database.core import execute_sql_from_string
+from src.utils.alerting import AlertLevel
+from src.utils.alerting import log_to_discord
 
+def run_dq_checks(db_pool: ConnectionPool, dataframe: pl.DataFrame) -> None:
+    pass
 
+def ensure_schema(db_pool: ConnectionPool) -> None:
+    """
+    A function that creates the tables automatically in postgres.
+
+    db_pool: The database connection pool.
+    """
+    classes = BaseIGDBSchema.__subclasses__()
+    
+    for cls in classes:
+        query: str = cls.build_pg_query()
+
+        execute_sql_from_string(pool=db_pool, query=query, is_multistatement=True)
+
+#TODO: on appelle cette fonction dans do ingestion si possible, ça sera un job séparé de blabla
 def ingest_batches_to_postgres(azure_client: DataLakeServiceClient | BlobServiceClient, 
                                db_pool: ConnectionPool
 ) -> None:
@@ -11,4 +34,7 @@ def ingest_batches_to_postgres(azure_client: DataLakeServiceClient | BlobService
     PLACEHOLDER — future implementation.
     Reads newly landed raw/bronze batches from ADLS and loads them into Postgres.
     """
-    pass
+
+    dataframe: pl.DataFrame
+    
+    ensure_schema(db_pool=db_pool)
--- src/igdb/models/__init__.py
+++ src/igdb/models/__init__.py
@@ -3,6 +3,24 @@ IGDB models - SSOT.
 
 Central export for all IGDB schemas. 
 Import from here, not from individual files.
+
+In case you want to add a new Class, it must be declared here. Or else, you won't see it in analytics
 """
 
 from .BASE import BaseIGDBSchema, BASE_IGDB_URL
+
+from .companies import CompanySchema
+from .games import GameSchema
+from .genres import GenreSchema
+from .platforms import PlatformSchema
+from .release_dates import ReleaseDateSchema
+
+__all__ = [
+    "BaseIGDBSchema"    ,
+    "CompanySchema"     ,
+    "GameSchema"        ,
+    "GenreSchema"       ,
+    "PlatformSchema"    ,
+    "ReleaseDateSchema" ,
+    # add here in case you create a new class...
+]
